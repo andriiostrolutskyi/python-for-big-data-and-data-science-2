9193a2e1adaae76b11dee3a476259b9aaeccf179
andriiostrolutskyi <andrii.ostrolutskyi@ukma.edu.ua>
2024-03-18 01:56:40 -0400

    Got rid of absolute paths

--- tests/test_io/test_input.py
+++ tests/test_io/test_input.py
@@ -2,16 +2,18 @@ import unittest
 import pandas as pd
 from app.io import input
 
+path_to_test_file = "test_data\\test.txt"
+path_to_csv_file = "test_data\\addresses.csv"
+
 
 class TestReadFromFile(unittest.TestCase):
-
     def test_file_existence(self):
-        result = input.read_from_file("C:\\Users\\user\\PycharmProjects\\Ostrolutskyi_python\\data\\test.txt")
+        result = input.read_from_file(path_to_test_file)
         self.assertNotEqual(result, "File not found.")
 
     def test_file_content(self):
         expected_content = "test"
-        result = input.read_from_file("C:\\Users\\user\\PycharmProjects\\Ostrolutskyi_python\\data\\test.txt")
+        result = input.read_from_file(path_to_test_file)
         self.assertEqual(result, expected_content)
 
     def test_nonexistent_file(self):
@@ -22,15 +24,15 @@ class TestReadFromFile(unittest.TestCase):
 class TestReadWithPandas(unittest.TestCase):
 
     def test_file_existence(self):
-        result = input.read_with_pandas("C:\\Users\\user\\PycharmProjects\\Ostrolutskyi_python\\data\\addresses.csv")
+        result = input.read_with_pandas(path_to_csv_file)
         self.assertNotEqual(result.to_string(), "File not found.")
 
     def test_dataframe_type(self):
-        result = input.read_with_pandas("C:\\Users\\user\\PycharmProjects\\Ostrolutskyi_python\\data\\addresses.csv")
+        result = input.read_with_pandas(path_to_csv_file)
         self.assertIsInstance(result, pd.DataFrame)
 
     def test_dataframe_columns(self):
-        result = input.read_with_pandas("C:\\Users\\user\\PycharmProjects\\Ostrolutskyi_python\\data\\addresses.csv")
+        result = input.read_with_pandas(path_to_csv_file)
         expected_columns = ["First Name", "Last Name", "Address", "City", "State", "Zip"]
         self.assertEqual(result.columns.tolist(), expected_columns)
 
